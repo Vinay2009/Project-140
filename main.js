25ba88ff23db2ccd6b17e1379256522587ca74ce
Vinay2009 <91785104+Vinay2009@users.noreply.github.com>
2022-08-27 17:59:07 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -25,13 +25,18 @@ function setup(){
   var canvas =  createCanvas(700,600);
   canvas = createCanvas(700, 600);
   canvas.position(500, 150);
-  video = createCapture(VIDEO);
+  var video = createCapture(VIDEO);
+  video.size(700,600);
+  video.hide();
+  canvas.parent("canvas");
 }
 
 
 function draw(){
 
- background(0); 
+  image(video, 0, 0, 700, 600);
+ 
+  background(0); 
 
  fill("black");
  stroke("black");
